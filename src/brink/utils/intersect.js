bd2414c52f355f55883117957e307116254c625d
Taka Kojima <taka@gigafied.com>
2014-02-20 13:58:40 -0800

    Add intersect utility method

--- src/brink/utils/intersect.js
+++ src/brink/utils/intersect.js
@@ -1,16 +1,28 @@
 $b(
 
     [
-
+        './flatten'
     ],
 
-    function () {
+    function (flatten) {
 
         'use strict';
 
-        return function () {
+        return function (a, b) {
 
+            var i,
+                c;
 
+            b = flatten([].slice.call(arguments, 1));
+            c = [];
+
+            for (i = 0; i < b.length; i ++) {
+                if (~a.indexOf(b[i])) {
+                    c.push(b[i]);
+                }
+            }
+
+            return c;
         };
     }
 
