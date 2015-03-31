da63e4fb5ef24331d209e20236d91f9e8632f4f3
Taka Kojima <taka@gigafied.com>
2015-03-31 14:18:42 -0700

    Add blanket for coverage testing...

---
+++ tasks/coverage.js
@@ -0,0 +1,1 @@
+require('../tests/coverage.js');
---
+++ tests/coverage.js
@@ -0,0 +1,65 @@
+var p,
+    fs,
+    path,
+    chai,
+    mocha,
+    timeout;
+
+require('blanket')({
+    'pattern' : 'src/brink',
+    'data-cover-never': [
+        'src/brink/polyfills',
+        'src/brink/node',
+        'src/brink/dom'
+    ]
+});
+require('../src/brink/brink.js');
+
+fs = require('fs');
+path = require('path');
+chai = require('chai'),
+mocha = require('mocha');
+
+mocha = new mocha({
+    ui : 'bdd',
+    reporter : 'html-file-cov'
+});
+
+global.expect = chai.expect;
+
+var done = function (failures) {
+    process.exit(failures);
+};
+
+function addTests(folder, p) {
+
+	fs.readdirSync(folder).filter(function (file) {
+
+		p = path.join(folder, file);
+
+		if (fs.statSync(p).isDirectory()) {
+			addTests(p);
+			return;
+		}
+
+		if (file === 'index.js') {
+			mocha.addFile(p);
+		}
+	});
+}
+
+module.exports = function (cb) {
+
+    if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+    }
+
+    mocha.run(cb);
+};
+
+addTests(path.join(__dirname, 'brink'));
+
+timeout = setTimeout(function () {
+    mocha.run(done);
+}, 0);
