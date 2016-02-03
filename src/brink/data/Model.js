3de5a6d5e43a0b37a1671e58f6e3f13212dadae6
Taka Kojima <taka@gigafied.com>
2016-02-02 16:06:43 -0800

    fix collectionKey bug...

--- src/brink/data/Model.js
+++ src/brink/data/Model.js
@@ -143,8 +143,6 @@ $b(
             @default null
             ************************************************************************/
 
-            collectionKey : null,
-
             /***********************************************************************
             The property name of the primaryKey you are using for this Model.
 
